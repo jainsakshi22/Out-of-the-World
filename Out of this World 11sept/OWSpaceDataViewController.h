fc74d1cc21c569958c2d562171a9641c2ecdeb5f
Sakshi Jain <sakshi@Sakshis-MacBook-Pro.local>
2014-09-20 13:30:57 +0530

    Added Another UITable part2

---
+++ Out of this World 11sept/OWSpaceDataViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWSpaceDataViewController.h
+//  Out of this World 11sept
+//
+//  Created by Sakshi Jain on 20/09/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceDataViewController : UIViewController <UITableViewDelegate,UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ Out of this World 11sept/OWSpaceDataViewController.m
@@ -0,0 +1,68 @@
+//
+//  OWSpaceDataViewController.m
+//  Out of this World 11sept
+//
+//  Created by Sakshi Jain on 20/09/14.
+//
+//
+
+#import "OWSpaceDataViewController.h"
+
+@interface OWSpaceDataViewController ()
+
+@end
+
+@implementation OWSpaceDataViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    self.tableView.backgroundColor = [UIColor clearColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark- UITableViewDataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"DataCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    cell.textLabel.text = @"Data";
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
